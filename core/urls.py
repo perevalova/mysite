236c73781eda1cc204c79c7eb51ab5c9306d4667
Anastasia Perevalova <perev4n@gmail.com>
2019-08-27 23:00:00 +0300

    Fixed urls

--- core/urls.py
+++ core/urls.py
@@ -8,9 +8,9 @@ from .views.main import HomePageView, AboutPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('contacts/', ContactView.as_view(), name='contacts'),
+    path('about', AboutPageView.as_view(), name='about'),
+    path('contacts', ContactView.as_view(), name='contacts'),
     # path('cv', CvPageView.as_view(), name='cv'),
     path('download/cv', download_file, name='download_pdf'),
-    path('projects/', ProjectPageView.as_view(), name='projects'),
+    path('projects', ProjectPageView.as_view(), name='projects'),
 ]
