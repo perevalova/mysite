9de57fbf9fac3e91bbcd21bd64302d7d00c74127
Anastasia Perevalova <perev4n@gmail.com>
2019-08-27 18:13:56 +0300

    Changed settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,14 +131,8 @@ LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'core/locale'),
 )
 
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = 'core/static/'
-
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
